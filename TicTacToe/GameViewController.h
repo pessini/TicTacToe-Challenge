e21b2cda4857eb083ec9660e07cf570efc3190f3
Leandro Pessini <pessini@Leandros-MacBook-Air.local>
2015-03-12 16:51:23 -0300

    Change the name of the ViewController

--- TicTacToe/GameViewController.h
+++ TicTacToe/GameViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  GameViewController.h
 //  TicTacToe
 //
 //  Created by Leandro Pessini on 3/12/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface GameViewController : UIViewController
 
 @end
-
---
+++ TicTacToe/GameViewController.m
@@ -0,0 +1,24 @@
+//
+//  GameViewController.m
+//  TicTacToe
+//
+//  Created by Leandro Pessini on 3/12/15.
+//  Copyright (c) 2015 Brazuca Labs. All rights reserved.
+//
+
+#import "GameViewController.h"
+
+@interface GameViewController ()
+
+@end
+
+@implementation GameViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+
+@end
